a7ae013ec5e3da02a87c72dfb5faa041e36d8fb3
Dario <dariochiodini@gmail.com>
2022-05-17 15:08:34 -0300

    1
    1

--- 2.py
+++
@@ -1,7 +0,0 @@
-#Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
-
-variable = "Hola Mundo!"
-print("el contenido de la variable es:",  variable)
-
-#https://github.com/dariochiodini/Python
-#Por Dario Chiodini
